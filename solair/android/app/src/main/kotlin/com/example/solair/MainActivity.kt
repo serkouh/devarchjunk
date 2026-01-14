c0e55af5fdd2d1ba2ece94548d1cff1f31ae05ce
Serkouh-Abderrahmane <serkouh.abderrahman@gmail.com>
2026-01-14 05:41:10 +0100

    Fix ignore rules: allow tennis2 and zakaat_calculator contents

---
+++ solair/android/app/src/main/kotlin/com/example/solair/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devarch.solair
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tennis/android/app/src/main/kotlin/com/example/tennis/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tennis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
